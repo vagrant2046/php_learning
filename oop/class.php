84d41cba67227552e0cd225af85f17a5509006f2
king <1020426908@qq.com>
2021-12-23 16:37:50 +0800

    PHP继承、封装、多态

--- oop/class.php
+++ oop/class.php
@@ -1,13 +1,17 @@
 <?php
+ini_set("display_errors", "On");
+
+error_reporting(E_ALL | E_STRICT);
 //PHP类与对象、访问控制
 class Car
 {
     #类属性
     const WHEELS = 4;//汽车都是4个轮子
-    protected $seats;   //座位
-    protected $doors;   //门
-    protected $engine;  //发动机
-    public $brand;   //品牌
+    #访问控制
+    public  $seats;   //座位
+    public  $doors;   //门
+    public  $engine;  //发动机
+    public  $brand;   //品牌
     #构造函数
     public function __construct($brand, $seats = 5, $doors = 4, $engine = 1)
     {
@@ -51,7 +55,49 @@ class Car
 // var_dump($car->getBrand());
 // $car->drive();
 // $car->close();
-$car = new Car("保时捷");
-var_dump($car->getBrand());
-var_dump($car->brand);
-#访问控制
+// $car = new Car("保时捷");
+// var_dump($car->getBrand());
+// var_dump($car->brand);
+#PHP继承、封装与多态
+// class Benz extends Car
+// {
+//     public function __construct($seats = 5, $doors = 4, $engine = 1)
+//     {
+//       $this->brand = '宝马';
+//       parent::__construct($this->brand,$seats,$doors,$engine);
+//     }
+// }
+// $benz = new Benz();
+// var_dump($benz->brand);
+// $benz->drive();
+#封装:不需要关心对象方法的实现细节
+#多态
+class Benz extends Car
+{
+    //重写父类实现
+    public function drive()
+    {
+        echo "22已经重写了！";
+        //parent::drive(); 
+    }
+}
+// $benz = new Benz('benz');
+// $benz->drive();
+#类型转换
+//不能将父类对象转换为子类对象
+class TestCarDrive
+{
+    public function testDrive(Car $car)
+    {
+        $car->drive();
+    }
+    public function testBenzDrive(Benz $benz)
+    {
+        $benz->drive();
+    }
+}
+$bmw = new Car('bmw');
+$benz = new Benz('benz');
+$test = new TestCarDrive();
+$test->testDrive($benz);
+$test->testBenzDrive($bmw);
